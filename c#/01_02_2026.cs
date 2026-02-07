61e2a3dde7e26123efe45e8b3d1245e46194cbf8
Kaio Vinícios <kvsgpro@outlook.com>
2026-02-06 23:16:41 -0300

    correct commit C# 01/02/2026 (b31173c)

--- c#/01_02_2026.cs
+++ c#/01_02_2026.cs
@@ -1,5 +1,17 @@
+/*
+292- Nim Game
+
+https://leetcode.com/problems/nim-game/
+
+Given n, the number of stones in the heap, return true if you can win the game assuming both you and your friend play optimally, otherwise return false.
+*/
+
 public class Solution {
     public bool CanWinNim(int n) {
-        
+        if(n % 4 == 0){
+          return false;
+        } else {
+          return true;
+        }
     }
 }
